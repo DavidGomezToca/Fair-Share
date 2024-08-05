37f2baefc080544a9152b017085b3161f12cbd5e
DavidGomezToca <DAVID_AGOMEZT@HOTMAIL.COM>
2024-08-05 22:43:09 +0100

    FAIR SHARE 0.5.0
    Add Feature Add Friends

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 
 - **STACK :**
 
-  - **Fair Share** : `0.4.0`
+  - **Fair Share** : `0.5.0`
   - **React** : `18.3.1`
   - **React Dom** : `18.3.1`
 
--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,23 @@ import FriendsData from "../data/friendsData.json";
 import VersionWatermark from "./VersionWatermark";
 
 export default function App() {
-  const [showAddFriend, setShowAddFriend] = useState(false);
+  const [friends, setFriends] = useState(FriendsData.friends);
+  const [showAddFriend, setShowAddFriend] = useState(true);
 
   function handleShowAddFriend() {
     setShowAddFriend((showAddFriend) => !showAddFriend);
   }
 
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+    setShowAddFriend(false);
+  }
+
   return (
     <div className='app'>
       <div className='sidebar'>
-        <FriendList />
-        {showAddFriend && <FormAddFriend />}
+        <FriendList friends={friends} />
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
         <Button onClick={handleShowAddFriend}>{showAddFriend ? "Close" : "Add friend"}</Button>
       </div>
       <FormSplitBill />
@@ -22,9 +28,7 @@ export default function App() {
   )
 }
 
-function FriendList() {
-  const friends = FriendsData.friends;
-
+function FriendList({ friends }) {
   return (
     <ul>{friends.map(friend => <Friend key={friend.id} friend={friend} />)}</ul >
   )
@@ -61,21 +65,39 @@ function Button({ children, onClick, disabled }) {
   )
 }
 
-function FormAddFriend() {
+function FormAddFriend({ onAddFriend }) {
+  const [name, setName] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name) return;
+
+    const id = crypto.randomUUID();
+    const newFriendName = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+    const newFriend = {
+      id,
+      name: newFriendName,
+      image: `https://i.pravatar.cc/48?${id}`,
+      balance: 0,
+    };
+
+    onAddFriend(newFriend);
+  }
+
   return (
-    <form className="form-add-friend">
-      <InputText>👬Friend Name</InputText>
-      <InputText>📷 Image URL</InputText>
-      <Button disabled={true}>Add</Button>
-    </form>
+    <form className="form-add-friend" onSubmit={handleSubmit}>
+      <InputText value={name} setValue={setName}>👬Friend Name</InputText>
+      <Button>Add</Button>
+    </form >
   )
 }
 
-function InputText({ children, disabled }) {
+function InputText({ children, value, setValue, disabled }) {
   return (
     <>
       <label>{children}</label>
-      <input type="text" disabled={disabled} />
+      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} disabled={disabled} />
     </>
   )
 }
@@ -86,8 +108,8 @@ function FormSplitBill() {
   return (
     <form className="form-split-bill">
       <h2>Split a bill with {selectedFriend}</h2>
-      <InputText>💰 Bill value</InputText>
-      <InputText>🙍‍♂️ Your expense</InputText>
+      <InputText disabled="disabled">💰 Bill value</InputText>
+      <InputText disabled="disabled">🙍‍♂️ Your expense</InputText>
       <InputText disabled="disabled">👬 {selectedFriend}'s expense</InputText>
       <InputSelect selectedFriend={selectedFriend} />
       <Button disabled={true}>Split bill</Button>
